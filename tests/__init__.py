9c10a8a31f9d46d55e57d595e3aae9e2ff515724
Josh <josh@joshthomas.dev>
2024-02-08 15:06:56 -0600

    add version to generating template

---
+++ tests/__init__.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+import logging
+
+
+pytest_plugins = []  # type: ignore
+
+
+def pytest_configure(config):
+    logging.disable(logging.CRITICAL)
---
+++ tests/test_version.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from pathlib import Path
+import yaml
+import toml
+
+BASE_DIR = Path(__file__).parent.parent
+
+def test_copier_yml_version():
+    file = BASE_DIR / "copier.yml"
+    with open(file, encoding="utf-8") as f:
+        copier_yml = yaml.safe_load(f)
+
+    version = copier_yml["template_version"]["default"]
+
+    assert version == "2024.1"
+    
+
+def test_pyproject_toml_version():
+    file = BASE_DIR / "pyproject.toml"
+    with open(file, encoding="utf-8") as f:
+        pyproject_toml = toml.load(f)
+
+    version = pyproject_toml["project"]["version"]
+
+    assert version == "2024.1"
