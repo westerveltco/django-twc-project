fa2d1a74f89dd32e1094c3c8031bebddbbbaba6b
Josh Thomas <jthomas@westervelt.com>
2024-04-05 14:03:00 -0500

    :bookmark: bump version 2024.13 -> 2024.14 (#109)

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,19 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import yaml
-
 BASE_DIR = Path(__file__).parent.parent
-
-
-def test_copier_yml_version():
-    file = BASE_DIR / "copier.yml"
-    with open(file, encoding="utf-8") as f:
-        copier_yml = yaml.safe_load(f)
-
-    version = copier_yml["template_version"]["default"]
-
-    assert version == "2024.13"
 
 
 def test_VERSION_version():
@@ -22,4 +10,4 @@ def test_VERSION_version():
     with open(file, encoding="utf-8") as f:
         version = f.read().strip()
 
-    assert version == "2024.13"
+    assert version == "2024.14"
