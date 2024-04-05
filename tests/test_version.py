777caeb803d0c9c4bff8901770b21171e199d90d
Josh <josh@joshthomas.dev>
2024-04-05 13:56:57 -0500

    remove test for copier answer

--- tests/test_version.py
+++ tests/test_version.py
@@ -2,19 +2,7 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import yaml
-
 BASE_DIR = Path(__file__).parent.parent
-
-
-def test_copier_yml_version():
-    file = BASE_DIR / "copier.yml"
-    with open(file, encoding="utf-8") as f:
-        copier_yml = yaml.safe_load(f)
-
-    version = copier_yml["template_version"]["default"]
-
-    assert version == "2024.14"
 
 
 def test_VERSION_version():
